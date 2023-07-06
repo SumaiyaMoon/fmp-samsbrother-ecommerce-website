20cac8d8b222e1d42c0a23820e83d90eba08c769
SumaiyaMoon <sumaiyaaltafmoon@gmail.com>
2023-07-06 18:08:21 +0500

    Ecommerce Website Sam's Brother

--- public/js pages/cart.js
+++ public/js pages/cart.js
@@ -28,3 +28,26 @@ var costSubTotal = document.getElementById("costSubTotal")
 var costTax = document.getElementById("costTax")
 var costDelivery = document.getElementById("costDelivery")
 var costTotal = document.getElementById("costTotal")
+
+
+
+//CartMain
+var cartMain = document.getElementById("cartMain");
+cartMain.innerHTML = ` <div class="container-fluid d-flex justify-content-center align-items-center">
+<div class="container bg_container_subtle border rounded shadow m-5 p-5">
+<h1>We are under maintenance.</h1>
+</div></div>`;
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var cartMain = document.getElementById("cartMain");
+//     if (cartMain) {
+//       cartMain.innerHTML = `
+//         <div class="container-fluid d-flex justify-content-center align-items-center">
+//           <div class="container bg_container_subtle border rounded shadow m-5 p-5">
+//             <h1>We are under maintenance.</h1>
+//           </div>
+//         </div>
+//       `;
+//     }
+//   });
+  
